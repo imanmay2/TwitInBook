580cf867d8c31ba486dfb0e024ed51bf28db3b89
manmay2 <imanmay2@gmail.com>
2024-12-25 00:25:31 +0530

    project initiated

---
+++ index.cjs
@@ -0,0 +1,21 @@
+let express=require("express");
+let app=express();
+const path=require("path");
+const port=16539;
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "/public")))
+
+
+
+app.listen(port,(req,res)=>{
+    console.log("Server is listening to : ",port);
+});
+
+
+
+app.get("/",(req,res)=>{
+    res.render("home.ejs");
+});
