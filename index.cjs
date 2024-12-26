bcdf35884749b9d64050e285bc21148fd567684c
manmay2 <imanmay2@gmail.com>
2024-12-26 12:01:56 +0530

    bug fixed

--- index.cjs
+++ index.cjs
@@ -1,13 +1,13 @@
 let express=require("express");
 let app=express();
 const path=require("path");
-const port=16539;
+const port=8080;
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"/views"));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "/public")))
-
+app.use(express.static(path.join(__dirname,"/images")))
 
 app.listen(port,(req,res)=>{
     console.log("Server is listening to : ",port);
