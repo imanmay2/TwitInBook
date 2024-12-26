38128f577c0a4296e182d29fca9c811878a0c83a
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2024-12-26 18:08:55 +0530

    icons added . and spaces reduced....

--- index.cjs
+++ index.cjs
@@ -1,7 +1,7 @@
 let express=require("express");
 let app=express();
 const path=require("path");
-const port=8080;
+const port=16539;
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"/views"));
 app.use(express.urlencoded({extended:true}));
